dd56ea4903d5ac97f32114530f1a1e74a1a5ffde
LeeJongBeom <52884648+devleejb@users.noreply.github.com>
2022-09-10 12:31:29 +0900

    First Commit

---
+++ src/test/java/devleejb/springstart/SpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package devleejb.springstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
